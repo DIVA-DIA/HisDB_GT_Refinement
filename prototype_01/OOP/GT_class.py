7342f5a526f9373cbcf706a898b1e04de96e3de0
Max Haller <loverboy99@Maxs-MacBook-Pro.local>
2021-12-02 16:23:40 +0100

    So these are the classes I talked about in the last commit

---
+++ prototype_01/OOP/GT_class.py
@@ -0,0 +1,13 @@
+# soll die Klassen des GT repräsentieren
+
+# TODO: Überlege, ob es Sinn macht die Klassen als classes zu speichern. Falls ja, den Code ergänzen.
+
+class GT_classes():
+    name = None
+    color = (0,0,0) # default color
+    def __init__(self, name):
+        self.name = name
+
+class decoration():
+    def __init__(self):
+        super(self, "decoration")
