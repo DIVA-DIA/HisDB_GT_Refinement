567cbd9570c4ec980b8ccd9511714638df783208
Max Haller <loverboy99@Maxs-MacBook-Pro.local>
2021-12-02 16:25:02 +0100

    Tried to write a method that makes all background pixels whit -> there are three versions of this method, but none works

--- prototype_01/Pixel_level_handler.py
+++ prototype_01/Pixel_level_handler.py
@@ -36,38 +36,44 @@ from PIL import Image
 from numpy import asarray
 import numpy as np
 
+
 # TODO: Als erstes werde ich ein pixel-base image lesen und eine geschickte Datenstruktur entwickeln.
+# TODO: Einen Algorithmus schreiben, der durch jedes Pixel im Bild durchgeht, es kategorisiert und überprüft, ob es in einem Polygon ist.
 
 
-def read_pixel_level_gt(image_path :Path):
+def read_pixel_level_gt(image_path: Path):
     # store png as numpy array
     # go through the numpy array and store the different colors
     rgb = Image.open(image_path).convert("RGB")
     img_asarray = asarray(rgb)
-    print(img_asarray.ndim)
-    print(img_asarray.shape)
+    img_asarray_to_white = set_background_to_white(img_asarray)
+    print(img_asarray_to_white.ndim)
+    print(img_asarray_to_white.shape)
+    print(get_unique_pixels(img_asarray_to_white))
+    print(rgb.getpixel((10, 23)))
 
 
-
-    # for height in img_asarray:
-    #     for width in height:
-    #         if width.any()
-    #         else:
-    #             unique_pixels.append(width)
-
-    print(get_unique_pixels(img_asarray))
-
-    print(rgb.getpixel((10,23)))
-
-
-def get_unique_pixels(img_asarray : np.ndarray):
+def get_unique_pixels(img_asarray: np.ndarray):
     return np.unique(img_asarray.reshape(-1, img_asarray.shape[2]), axis=0)
 
 
+# make all background pixels white
+def set_background_to_white(img_asarray: np.ndarray):
+    for y in img_asarray:
+        for x in y:
+            img_asarray[y][x] = np.where(img_asarray[y][x] == [0,0,1], [255,255,255],img_asarray[y][x])
+    return img_asarray
 
+def set_background_to_white(img_asarray: np.ndarray):
+    for y in img_asarray:
+        for x in y:
+            if img_asarray[y][x] == [0,0,1]:
+                img_asarray[y][x] = [255,255,255]
+    return img_asarray
 
-
+def set_background_to_white(img_asarray: np.ndarray):
+    return [[color := [255,255,255] for color in x if color == [0,0,1]] for x in img_asarray]
 
 if __name__ == '__main__':
-    pixel_level_gt = Path("../CB55/pixel-level-gt/public-test/e-codices_fmb-cb-0055_0098v_max.png")
+    pixel_level_gt = Path("../CB55/pixel-level-gt/public-test/e-codices_fmb-cb-0055_0102v_max.png")
     read_pixel_level_gt(pixel_level_gt)
