ae5692e4c588bd371a65049815ad69670336cfb6
max3324 <mahaller88>
2022-03-01 11:55:48 +0100

    NEW PROTOTYPE now works

--- prototype_02/Classes/Main.py
+++ prototype_02/Classes/Main.py
@@ -9,15 +9,16 @@ from HisDB_GT_Refinement.prototype_02.Classes.PageOntoImage import overlay_img_w
 from HisDB_GT_Refinement.prototype_02.Classes.PixelResizer import MinorityWins, NaiveResizer, MajorityWins
 from datetime import datetime
 
-RESIZE_FACTOR = 4 # cannot be 3
+RESIZE_FACTOR = 4  # cannot be 3
 RESIZING_STRATEGY = MajorityWins()
 
 
 def open_images(directory: Path):
     img = []
     dir = sorted(os.listdir(directory))
+    print(dir)
     for file in dir:
-        img.append(Image.open(Path(directory/file)).convert("RGB"))
+        img.append(Image.open(Path(directory / file)).convert("RGB"))
     return img
 
 
@@ -40,7 +41,7 @@ def save_images_as(images, OutputDirectory: Path, format: str):
     now = datetime.now().strftime("%H_%M_%S")
     i = 0
     for img in images:
-        fp = "{}/picture_no_{}_{}.{}".format(OutputDirectory, i,now, format.lower())
+        fp = "{}/picture_no_{}_{}.{}".format(OutputDirectory, i, now, format.lower())
         i = i + 1
         img.save(fp=fp)
 
@@ -55,23 +56,36 @@ if __name__ == '__main__':
 
     # output
     intermediate_result = Path("../Output/GT_1/Resized_PX_Based_GT/")
-    output_path = Path("../Output/GT_1/FINAL_GT_outline_2px_strategy_majority_wins/")
+    output_path = Path("../Output/GT_1/FINAL_GT_outline_1px_strategy_majority_wins/")
 
     # get all images
     images = open_images(pixel_level_gt)
 
+    # get all xml paths and store them as polygons
+    # TODO: refine MyPolygon to store the polygons with their original values, center=FALSE, add method to resize,
+    #  also handy to display boundary boxes
+    path_to_xml_gt: List[str] = sorted(os.listdir(xml_gt))
+    print(path_to_xml_gt)
+
+
+
     # resize all pixel based images and save them as pixel_based_GT
     resized = resize_px_images(images=images)
-    save_images_as(images=resized, OutputDirectory=intermediate_result, format="png")
+    #save_images_as(images=resized, OutputDirectory=intermediate_result, format="png")
 
     # get directories of the new intermediate result and
-    path_to_pixel_based_gt: List[str] = sorted(os.listdir(intermediate_result))
-    path_to_xml_gt: List[str] = sorted(os.listdir(xml_gt))
-    assert len(path_to_pixel_based_gt) == len(path_to_xml_gt)
+    #path_to_pixel_based_gt: List[str] = sorted(os.listdir(intermediate_result))
+    #assert len(path_to_pixel_based_gt) == len(path_to_xml_gt)
 
+
+    # TODO: create class or script called OverLayer -> combines the lists and does logic on them
     # call the overlay_xml_with_img in PageOntoImage that resizes the polyons
-    for i in range(len(path_to_pixel_based_gt)):
-        overlay_img_with_xml(image_path=Path(intermediate_result/path_to_pixel_based_gt[i]),
-                             xml_path=Path(xml_gt/path_to_xml_gt[i]), output_path=output_path,
-                             resize_factor=RESIZE_FACTOR)
+    i = 0
+    final_images = []
+    for img in resized:
+        final_images.append(overlay_img_with_xml(img,
+                             xml_path=Path(xml_gt / path_to_xml_gt[i]), output_path=output_path,
+                             resize_factor=RESIZE_FACTOR))
+        i = i + 1
+    save_images_as(final_images,output_path,"gif")
 
--- prototype_02/Classes/PageOntoImage.py
+++ prototype_02/Classes/PageOntoImage.py
@@ -14,35 +14,28 @@ from HisDB_GT_Refinement.prototype_02.Classes.ColorPalette import create_color_p
 # draws the polygons onto the image
 from HisDB_GT_Refinement.prototype_02.Classes.MyPolygon import Polygon
 
-
-def overlay_img_with_xml(image_path: Path, xml_path: Path, output_path: Path, resize_factor):
+def overlay_img_with_xml(image: Image, xml_path: Path, output_path: Path, resize_factor):
     output_path.mkdir(exist_ok=True)
     polygons = get_polygons_from_xml(xml_path=xml_path)
-    #print("polygons: " + str(polygoRns))
-    resized_polygons = rescale_all_polygons(polygons,resize_factor)
-    #print("resized_polygons: " + str(resized_polygons))
-    # colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (255,255,255)]
+    resized_polygons = rescale_all_polygons(polygons, resize_factor)
     colors = create_color_palette(len(resized_polygons))
-    #print("In this file there are " + str(len(colors)) + " different colors possible of which " + str(len(resized_polygons)) + " are used")
-    #print(colors)
-    with Image.open(str(image_path)) as f:
-        for i, polygon in enumerate(resized_polygons):
-            img = ImageDraw.Draw(f)
-            #color = colors[i % len(colors)]
-            color = colors[i]
-            img.line(polygon, fill=color, width=2)
-            #img.polygon(polygon, fill = color, outline = color)
-        f.save(fp=Path(output_path / str(image_path.stem + "_every_line_different_and_resized_220226.gif")))
+    for i, polygon in enumerate(resized_polygons):
+        drawn_image = ImageDraw.Draw(image)
+        color = colors[i]
+        drawn_image.line(polygon, fill=color, width=1)
+        # img.polygon(polygon, fill = color, outline = color)
+    return image
 
 
-    # TODO: Get the different classes (comment, decoration, main-text) if necessary.
-    # get the polygons from the xml
+# TODO: Get the different classes (comment, decoration, main-text) if necessary.
+# get the polygons from the xml
 def get_polygons_from_xml(xml_path: Path):
     patt = re.compile('\{.*\}')
     # load xml
     tree = ET.parse(str(xml_path))
     root = tree.getroot()
-    ns = patt.match(root.tag).group() #group returns the different submatches of the regex match (https://www.geeksforgeeks.org/re-matchobject-group-function-in-python-regex/)
+    ns = patt.match(
+        root.tag).group()  # group returns the different submatches of the regex match (https://www.geeksforgeeks.org/re-matchobject-group-function-in-python-regex/)
     page_part = root[1]
     polygons = []
     # parse out the polygons
@@ -54,8 +47,11 @@ def get_polygons_from_xml(xml_path: Path):
     return polygons
 
     # TODO: write or find function that saves polygons as PAGE format
+
+
 def write_polygons_to_xml(xml_path: Path):
     pass
+
 
 def rescale_all_polygons(polygons, resize_factor):
     resized_polygons = []
@@ -83,16 +79,15 @@ if __name__ == '__main__':
     all_different_colors = Path("../Input/with_all_different_colors_02.png")
     output = Path("../Output/polygons_different_colors_26.2/")
 
-
     # CB55 img public test with PAGE-gt
-    #overlay_img_with_xml(original_png,xml_gt,output) # ever line a different color
+    # overlay_img_with_xml(original_png,xml_gt,output) # ever line a different color
 
     # CB55 pixel-level-gt
     overlay_img_with_xml(pixel_level_gt, xml_gt, output)
 
-    gif = Path(output/"e-codices_fmb-cb-0055_0098v_max_colored.gif")
+    gif = Path(output / "e-codices_fmb-cb-0055_0098v_max_colored.gif")
     rgb = Image.open(gif).convert("RGB")
-    #rgb.show(title="Original image (before conversion)")
+    # rgb.show(title="Original image (before conversion)")
     img_as_array = np.asarray(rgb)
     # convert the image to an index based image
     pil_image = Image.fromarray(img_as_array).convert('P', palette=Image.ADAPTIVE, colors=256)
