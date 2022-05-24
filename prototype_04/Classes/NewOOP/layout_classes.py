05fee8992176bf81711d62db3a05f17d74ef224f
max3324 <mahaller88>
2022-05-24 19:29:51 +0200

    LayoutClasses speichert nun alle Klassen inklusiver der COMMENT_AND_MAINTEXT Klasse (=10), welche Lars nicht in seiner Aufzählung aufgelistet hat, warum? Ferner überschreibt die LayoutClasses Klasse die __str__ methode, um eine schönere Illustration zu gewährleisten.

---
+++ prototype_04/Classes/NewOOP/layout_classes.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class LayoutClasses(Enum):
+    """
+    Represents the different classes of the pixel ground truth. Is useful for readability and implementation of
+    Layering.
+    """
+    BACKGROUND = 1
+    COMMENT = 2
+    DECORATION = 4
+    COMMENT_AND_DECORATION = 6
+    MAINTEXT = 8
+    COMMENT_AND_MAINTEXT = 10 # could either
+    MAINTEXT_AND_DECORATION = 12
+
+    def __str__(self):
+        return "%s: %s" % (self._name_, self._value_)
